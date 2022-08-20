2e404c2c15df388216df18f46196eab74e2b8e9f
Lario <osunkunleolanrewaju@gmail.com>
2022-08-20 05:54:12 +0100

    added a custom button and live form validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,23 @@
 import {useState} from 'react'
+import Button from './shared/Button'
 import Card from './shared/Card'
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if(text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        }else if (text !== '' && text.trim().length <= 10){
+            setBtnDisabled(true)
+            setMessage('text must be atleast 10 characters')
+        }else{
+            setBtnDisabled(false)
+            setMessage(null)
+        }
         setText(e.target.value)
     }
   return (
@@ -13,8 +26,10 @@ function FeedbackForm() {
             <h2>How would you rate our services with you?</h2>
             <div className="input-group">
                 <input type="text" placeholder='Write a review' onChange={handleTextChange}/>
-                <button type="submit">Send</button>
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
             </div>
+
+            {message && <div className='message'>{message}</div>}
         </form>
     </Card>
   )
