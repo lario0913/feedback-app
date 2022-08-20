ef2e107558525b6fe10ba42d7f657b88e91af092
Lario <osunkunleolanrewaju@gmail.com>
2022-08-20 04:49:59 +0100

    created the feedbackfrom components that will be used to add feedbacks comments to the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import FeedbackForm from './components/FeedbackForm'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import Header from './components/Header'
@@ -16,6 +17,7 @@ function App () {
     <>
       <Header/>
       <div className='container'>
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
       </div>
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,23 @@
+import {useState} from 'react'
+import Card from './shared/Card'
+
+function FeedbackForm() {
+    const [text, setText] = useState('')
+
+    const handleTextChange = (e) => {
+        setText(e.target.value)
+    }
+  return (
+    <Card>
+        <form>
+            <h2>How would you rate our services with you?</h2>
+            <div className="input-group">
+                <input type="text" placeholder='Write a review' onChange={handleTextChange}/>
+                <button type="submit">Send</button>
+            </div>
+        </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
