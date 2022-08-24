110da5db726cc4b8a5f5c7968771cd1ffa4490f6
Lario <osunkunleolanrewaju@gmail.com>
2022-08-24 17:37:04 +0100

    added about page using react-router-dom

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import Card from "../components/shared/Card"
+
+
+function AboutPage() {
+  return (
+    <Card className='about'>
+        <h2>About this project</h2>
+        <p>Version 1.0.0</p>
+
+        <a href="/">Back to homepage</a>
+    </Card>
+  )
+}
+
+export default AboutPage
