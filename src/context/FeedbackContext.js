b733f214b9642d50934b0eaca7f6cb9a3af7a551
Lario <osunkunleolanrewaju@gmail.com>
2022-08-27 16:26:55 +0100

    enables the updating and deleting the feedbacks on the feedback app from ther server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -23,13 +23,25 @@ export const FeedbackProvider = ({children}) => {
     }
 
 // update feedback
-    const updateFeedback = (id, updItem) => {
-      setFeedback(feedback.map(item => item.id === id ? {...item, ...updItem} : item))
+    const updateFeedback = async (id, updItem) => {
+      const response = await fetch(`/feedback/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updItem)
+      })
+      const data = await response.json()
+      setFeedback(feedback.map(item => item.id === id ? {...item, ...data} : item))
     }
 
 // delete feedback
-    const deleteFeedback = id => {
+    const deleteFeedback = async id => {
+
         if(window.confirm('Are you sure you want to delete?')){
+          await fetch(`/feedback/${id}`, {
+            method: 'DELETE'
+          })
           setFeedback(feedback.filter(item => item.id !== id))
         }
       }
