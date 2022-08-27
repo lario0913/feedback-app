caf59526fa378c7810fd49ded8f3763b274dc03d
Lario <osunkunleolanrewaju@gmail.com>
2022-08-27 14:39:35 +0100

    fetch feedback data from data and set up a spinner component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,31 +2,35 @@ import {motion, AnimatePresence} from 'framer-motion'
 import { useContext } from 'react'
 import FeedbackItem from "./FeedbackItem"
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext)
-    if(!feedback || feedback.length === 0){
+    const {feedback, isLoading} = useContext(FeedbackContext)
+
+    if(!isLoading && (!feedback || feedback.length === 0)){
         return <p>No Feedback Yet</p>
     }
-    return (
-        <div className="feedback-list">
-                <AnimatePresence>
-                {feedback.map((item) => (
-                    <motion.div 
-                      key={item.id}
-                      initial={{opacity:0}}
-                      animate={{opacity:1}}
-                      exit={{opacity:0}}
-                      >
-                        <FeedbackItem 
-                            item={item} 
+    return isLoading ? 
+            <Spinner /> : 
+            (
+                <div className="feedback-list">
+                        <AnimatePresence>
+                        {feedback.map((item) => (
+                            <motion.div 
                             key={item.id}
-                       
-                        />
-                    </motion.div>
-                ))}
-            </AnimatePresence>
-        </div>
+                            initial={{opacity:0}}
+                            animate={{opacity:1}}
+                            exit={{opacity:0}}
+                            >
+                                <FeedbackItem 
+                                    item={item} 
+                                    key={item.id}
+                            
+                                />
+                            </motion.div>
+                        ))}
+                    </AnimatePresence>
+                </div>
     )
 }
 
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,47 +1,47 @@
-import {createContext, useState} from 'react'
+import {createContext, useState, useEffect} from 'react'
 import {v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id : 1,
-            text: "This is the feedback item 1",
-            rating : 10,
-        },
-        {
-          id : 2,
-          text: "This is the feedback item 2",
-          rating : 10,
-      },
-      {
-        id : 3,
-        text: "This is the feedback item 3",
-        rating : 10,
-    }
-    ])
-
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({
       item: {},
       edit: false
     })
 
+    useEffect(() => {
+      fetchFeedback()
+    }, [])
+
+// Fetch feedback
+    const fetchFeedback = async () => {
+      const response =  await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+      const data = await response.json()
+      setFeedback(data)
+      setIsLoading(false)
+    }
+
+// update feedback
     const updateFeedback = (id, updItem) => {
       setFeedback(feedback.map(item => item.id === id ? {...item, ...updItem} : item))
     }
 
+// delete feedback
     const deleteFeedback = id => {
         if(window.confirm('Are you sure you want to delete?')){
           setFeedback(feedback.filter(item => item.id !== id))
         }
       }
 
+// Add feedback
     const addFeedback = (newFeedback) => {
       newFeedback.id = uuidv4()
       setFeedback([newFeedback, ...feedback])
     }
 
+// edit feedback
     const editFeedback = item => {
       setFeedbackEdit({
         item,
@@ -53,6 +53,7 @@ export const FeedbackProvider = ({children}) => {
               value={{
                 feedback,
                 feedbackEdit,
+                isLoading,
                 deleteFeedback,
                 addFeedback,
                 editFeedback,
